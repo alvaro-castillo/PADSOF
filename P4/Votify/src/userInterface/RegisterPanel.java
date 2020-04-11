4866bdd03eba2f1345e3a9f1fc7c5a6b131f5284
Miguel-VVV <60744297+Miguel-VVV@users.noreply.github.com>
2020-04-11 22:01:17 +0200

    Register Panel

---
+++ P4/Votify/src/userInterface/RegisterPanel.java
@@ -0,0 +1,95 @@
+import java.awt.*;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+
+
+public class RegisterPanel extends JPanel{
+
+    private static final long serialVersionUID=1L;
+    private JLabel labelRegister = new JLabel("Register Screen");
+    private JLabel labelUsername = new JLabel("Username:");
+    private JLabel labelNationalID = new JLabel("National ID:");
+    private JLabel labelPassword = new JLabel("Password:");
+    private JTextField usernameField = new JTextField(30);
+    private JTextField nationalIDField = new JTextField(30);
+    private JPasswordField passwordField = new JPasswordField(30);
+    private JButton createButton = new JButton("Create Account");
+
+    public RegisterPanel(){
+        
+    	this.setBorder(new EmptyBorder(20,20,20,20));
+    	this.setLayout(new GridBagLayout());
+    	
+    	GridBagConstraints c = new GridBagConstraints();
+    	
+    	c.fill = GridBagConstraints.NONE;
+    	c.weightx = 1.0;
+        c.weighty = 1.0;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        
+        labelRegister.setFont(new Font(labelRegister.getFont().getName(), Font.PLAIN, 30));
+        
+        int yPos = 0;
+        
+        c.ipady = 0;
+        c.gridy = yPos;
+        yPos++;
+        this.add(labelRegister, c);
+        
+        c.gridy = yPos;
+        yPos++;
+        this.add(new JSeparator(JSeparator.VERTICAL), c);
+        
+        c.gridy = yPos;
+        yPos++;
+        this.add(labelUsername, c);
+        
+        c.ipady = 10;
+        c.gridy = yPos;
+        yPos++;
+        this.add(usernameField, c);
+        
+        c.ipady = 0;
+        c.gridy = yPos;
+        yPos++;
+        this.add(new JSeparator(JSeparator.VERTICAL), c);
+        
+        c.gridy = yPos;
+        yPos++;
+        this.add(labelNationalID, c);
+        
+        c.ipady = 10;
+        c.gridy = yPos;
+        yPos++;
+        this.add(nationalIDField, c);
+        
+        c.ipady = 0;
+        c.gridy = yPos;
+        yPos++;
+        this.add(new JSeparator(JSeparator.VERTICAL), c);
+        
+        c.gridy = yPos;
+        yPos++;
+        this.add(labelPassword, c);
+        
+        c.ipady = 10;
+        c.gridy = yPos;
+        yPos++;
+        this.add(passwordField, c);
+        
+        c.ipady = 0;
+        c.gridy = yPos;
+        yPos++;
+        this.add(new JSeparator(JSeparator.VERTICAL), c);
+        
+        c.gridy = yPos;
+        yPos++;
+        this.add(createButton, c);
+        
+        
+    }
+}
+
