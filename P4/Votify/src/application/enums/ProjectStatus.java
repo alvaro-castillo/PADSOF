130cd39bec06184f9a13b6a8b613a8446839eef6
Beni7325 <34840974+Beni7325@users.noreply.github.com>
2020-04-25 14:20:41 +0200

    Añadido to string
    Para k salgan mejor los nombres en projectpanel

--- P4/Votify/src/application/enums/ProjectStatus.java
+++ P4/Votify/src/application/enums/ProjectStatus.java
@@ -1,15 +1,38 @@
 package application.enums;
+
 /**
 * These are the values which the Project Status can take, the ProjectSatus enum. 
 *
-* @author Miguel Álvarez Valiente, Alejandro Benimeli Miranda, Álvaro Castillo García
+* @author Miguel Ã�lvarez Valiente, Alejandro Benimeli Miranda, Ã�lvaro Castillo GarcÃ­a
 * */
 public enum ProjectStatus {
-	WAITING_ACCEPTANCE,
-	ADMIN_ACCEPTED,
-	ADMIN_REJECTED,
-	PENDING,
-	EXPIRED,
-	REJECTED,
-	APPROVED	
+	WAITING_ACCEPTANCE("Waiting Admin Acceptance"),
+	ADMIN_ACCEPTED("Accepted by Admin"),
+	ADMIN_REJECTED("Rejected by Admin"),
+	PENDING("Pending"),
+	EXPIRED("Expired"),
+	REJECTED("Rejected"),
+	APPROVED("Approved");
+	
+	/**
+	 * Description of each enumeration member
+	 */
+	private String descr;
+	
+	/**
+	 * Constructor of ProjectStatus
+	 * @param descr description of the enum member
+	 */
+	private ProjectStatus(String descr) {
+		this.descr = descr;
+	}
+	
+	/**
+	 * toString override
+	 */
+	@Override
+	public String toString() {
+		return descr;
+	}
+	
 }
